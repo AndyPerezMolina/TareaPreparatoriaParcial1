5a992f3bbc174e16db3282f2b526f362fb1da24f
AndyPerezMolina <andygudielperez@gmail.com>
2022-02-22 22:21:37 -0600

    version 0.1

---
+++ tresnumeros.py
@@ -0,0 +1,12 @@
+
+try :
+    print("Ingresar Numero 1")
+    num1 = float(input())
+
+    print("Ingresar Numero 2")
+    num2 = float(input())
+
+    print("Ingresar Numero 3")
+    num3 = float(input())
+except :
+    print("Debe ingresar un numero ")
